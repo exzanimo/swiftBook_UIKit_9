53fea73bb95251d88c865cd178d9fd69b8750d8f
Aleksandr Tarasov (exzanimo) <exzanimo@gmail.com>
2019-05-16 22:48:15 +0300

    Initial Commit

---
+++ UI_Course9 (UIContainerView)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_Course9 (UIContainerView)
+//
+//  Created by Александр Тарасов on 16/05/2019.
+//  Copyright © 2019 Aleksandr Tarasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
